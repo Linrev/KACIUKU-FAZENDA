9729c04b41c1d5c3b762c1d85008da22ebddd109
ryselis <djryse@gmail.com>
2014-06-16 21:05:51 +0300

    commit

--- public_html/naujas_patiekalas.php
+++ public_html/naujas_patiekalas.php
@@ -1,5 +1,11 @@
 <?php
 include("include/session.php");
+if (isset($_POST['title']) && isset($_POST['ingredients']) && isset($_POST['category'])){
+    $sql = 'INSERT INTO food (title, ingredients, category) VALUES (';
+    $sql .= '"'.$_POST['title'].'",'.'"'.$_POST['ingredients'].'",'.'"'.$_POST['category'].'");';
+    mysql_query($sql);
+    header( 'Location: meniu.php' ) ;
+}
 ?>
 
 <html>
@@ -15,7 +21,7 @@ include("include/session.php");
             form {background-color:#F1FCA2;}
             body {background-color:#F1FCA2;}
         </style>
-        <form>
+        <form method="POST">
             <div>
                 <label for="title">Pavadinimas</label>
                 <input type="text" id="title" name="title"/>
@@ -31,7 +37,7 @@ include("include/session.php");
                         $sql = "SELECT title FROM category;";
                         $res = mysql_query($sql);
                         while ($row = mysql_fetch_array($res)){
-                            echo '<option value="'.$row['title'].'">'.$row['title'].'<option>';
+                            echo '<option value="'.$row['title'].'">'.$row['title'].'</option>';
                         }
                     ?>
                 </select>
